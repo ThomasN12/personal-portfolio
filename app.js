41b3f8ad80e5e5cba4558aa33d384981dde8c733
ThinhNguyen <phuocthinhdn12023@gmail.com>
2022-03-23 01:43:30 -0400

    Updates

---
+++ app.js
@@ -0,0 +1,40 @@
+const menu = document.querySelector('nav .container ul');
+
+const navItems = menu.querySelectorAll('li');
+
+
+const removeAllActive = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+    })
+}
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', () => {
+        removeAllActive();
+        link.classList.add('active');
+    })
+})
+
+//read more
+const readMoreBtn = document.querySelector('.read-more');
+const readMoreContent = document.querySelector('.read-more-content');
+
+readMoreBtn.addEventListener('click', () => {
+    readMoreContent.classList.toggle('show-content');
+    if(readMoreContent.classList.contains('show-content')) {
+        readMoreBtn.textContent = "Show less";
+    } else {
+        readMoreBtn.textContent = "Show more";
+    }
+})
+
+//show/hide skills
+const skillItems = document.querySelectorAll('section.skills .skill');
+
+skillItems.forEach(skill => {
+    skill.querySelector('.head').addEventListener('click', () => {
+        skill.querySelector('.items').classList.toggle('show-items');
+    })
+})
